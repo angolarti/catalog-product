2a34e9c37ea59ad8cc72c4b677c192a0c16360c6
Walter Angolar <walter.silva@tistech.co.ao>
2022-12-07 08:16:29 +0100

    Add products route file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 
+import productsRoute from './routes/product.route.js'
+
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || '127.0.0.1'
 
 
 const app = express()
 app.use(handler)
+app.use(productsRoute)
 
 async function handler(req, res, next) {
     next()
